c3d4ad54b7b05aa9ec5b3e12fe86962c49de426e
tusharlock10 <tusharlock10@gmail.com>
2018-12-30 17:50:00 +0530

    Update TicTcaTocUltra.py

--- TicTcaTocUltra.py
+++ TicTcaTocUltra.py
@@ -22,33 +22,6 @@ def cycle(L):
     L=L+[x]
     return L
 
-def transform(color1, color2, skipR=1, skipG=1, skipB=1):
-    # This function transforms color1 into color2
-    L=[] 
-    if (color1[0]<color2[0]):i=list(range(color1[0], color2[0]+1, skipR))
-    else:i=list(range(color2[0], color1[0]+1, skipR))[::-1]
-    if i==[]:i=[color1[0]]
-  
-    if (color1[1]<color2[1]):j=list(range(color1[1], color2[1]+1, skipG))
-    else:j=list(range(color2[1], color1[1]+1, skipG))[::-1]
-    if j==[]:j=[color1[1]]
-
-    if (color1[2]<color2[2]):k=list(range(color1[2], color2[2]+1, skipB))
-    else:k=list(range(color2[2], color1[2]+1, skipB))[::-1]
-    if k==[]:k=[color1[2]]
-
-    x=max(len(i), len(j), len(k))
-    for m in range(len(i), x):i+=[i[-1]]
-    for m in range(len(j), x):j+=[j[-1]]
-    for m in range(len(k), x):k+=[k[-1]]
-
-    for m in range(x):
-        l=[i[m], j[m], k[m]]
-        L+=[l]
-    return L
-
-print(transform([0,0,0], [10,10,10]))
-
 def VIBGYOR():
     V=(128,0,128)
     I=(75,0,130)
@@ -57,26 +30,30 @@ def VIBGYOR():
     Y=(255,255,0)
     O=(255,165,0)
     R=(255,0,0)
-    L=transform(V,I,2)+transform(I,B,2)+transform(B,G,2)+transform(G,Y,2)+transform(Y,O,2)+transform(O,R,2)+transform(R,V,2)
+    L=tj.transform_color(V,I,2)+tj.transform_color(I,B,2)+tj.transform_color(B,G,2)+tj.transform_color(G,Y,2)+tj.transform_color(Y,O,2)+tj.transform_color(O,R,2)+tj.transform_color(R,V,2)
     return L
 
    
 AllColors=VIBGYOR()
 
-AllColors2=transform([255,0,0], [0,255,0])+transform([0,255,0], [0,0,255])+transform([0,0,255], [255,0,0])
+AllColors2=tj.transform_color([255,0,0], [0,255,0])+tj.transform_color([0,255,0], [0,0,255])+tj.transform_color([0,0,255], [255,0,0])
 
 
 
 
 class TicTacToe:
     global tile_len
+    tl=tile_len
 
     def __init__(self, RES):
+        tl=tile_len
         self.L = self.getList()
+        self.Big_L=[" "," "," "," "," "," "," "," "," "]
         self.grid = []  # self.makeGrid()
         self.currentBigTile = -1
         self.color=AllColors[0]
-        self.gridColor=AllColors2[0]
+        self.gridColor1=AllColors2[0]
+        self.gridColor2=AllColors2[50]
         self.Dict = {
             0: [0, 1, 2, 9, 10, 11, 18, 19, 20],
             1: [27, 28, 29, 36, 37, 38, 45, 46, 47],
@@ -88,9 +65,10 @@ class TicTacToe:
             7: [33, 34, 35, 42, 43, 44, 51, 52, 53],
             8: [60, 61, 62, 69, 70, 71, 78, 79, 80]}
 
-        self.bigTiles = {0: [0, 0], 1: [300, 0], 2: [600, 0],
-                         3: [0, 300], 4: [300, 300], 5: [600, 300],
-                         6: [0, 600], 7: [300, 600], 8: [600, 600]}
+        self.bigTile_coord={0:[0,0], 1:[tl*3, 0], 2:[tl*6, 0],
+                            3:[0,tl*3], 4:[tl*3, tl*3], 5:[tl*6, tl*6],
+                            6:[0,tl*6], 7:[tl*3, tl*6], 8:[tl*6, tl*6]}
+
     @staticmethod
     def __transpose(i):
         d={0:0, 1:3, 2:6, 3:1, 4:4, 5:7, 6:2, 7:5, 8:8}
@@ -124,29 +102,38 @@ class TicTacToe:
         tl = tile_len
         color=TILE_ACTIVE
 
-        for i in bigTileList:
-            Tile=self.grid[i]
+        for i in range(len(bigTileList)):
+            Tile_Num=bigTileList[i]
+            
+            if self.Big_L[self.__transpose(i)]!=" ":
+                continue
+            
+            Tile=self.grid[Tile_Num]
             coord=[Tile.x, Tile.y]
             Tile = pygame.draw.rect(screen, color, [coord[0], coord[1], tl, tl])
-            Type=self.L[i]
-            if Type=="X":self.draw_X(i)
-            if Type=="O":self.draw_O(i)
+            Type=self.L[Tile_Num]
+            if Type=="X":self.draw_X(Tile_Num)
+            if Type=="O":self.draw_O(Tile_Num)
 
 
 
     def makeGridLines(self, screen):
-        color=self.gridColor
+        
         tl = tile_len
         for i in range(1, 9):
             width = 2
+            color=self.gridColor1
             if i % 3 == 0:
-                width = 4
+                width = 8
+                color=self.gridColor2
             pygame.draw.line(screen, color, [i * tl, 0], [i * tl, RES[0]], width)
 
         for j in range(1, 9):
             width = 2
+            color=self.gridColor1
             if j % 3 == 0:
-                width = 4
+                width = 8
+                color=self.gridColor2
             pygame.draw.line(screen, color, [0, j * tl], [RES[0], j * tl], width)
 
     def check_collision(self, mouse_pos, mouse_click):
@@ -158,18 +145,17 @@ class TicTacToe:
             if Tile.collidepoint(mouse_pos):
                 return [True, i]
 
-    def draw_X(self, Tile_Num, clock=None):  # i is the tile number   
-        tl = tile_len
+    def draw_X(self, Tile_Num, clock=None, m=1):  # i is the tile number   
+        tl = tile_len*m
         bigTile_Num=self.teller(Tile_Num)
         color2=TILE_INACTIVE
-        self.L[Tile_Num]="X"
-        
         if bigTile_Num==self.currentBigTile:color2=TILE_ACTIVE
+
+        if m!=1:color2=TILE_INACTIVE
             
         Tile = self.grid[Tile_Num]
         a = (tl // 2) - 10
         center = [int(Tile.x + (tl // 2)), int(Tile.y + (tl // 2))]
-        #T.drawActiveTiles()
         for i in range(0,(tl // 2) - 10,2):
             c1 = [center[0] - i, center[1] - i]
             c2 = [center[0] + i, center[1] + i]
@@ -179,44 +165,64 @@ class TicTacToe:
             pygame.draw.line(screen, self.color, c3, c4, 7)
             pygame.draw.circle(screen, color2, center, a, 6)
             if clock!=None:
-                T.makeGridLines(screen)
-                #T.drawActiveTiles()
                 pygame.display.update()
                 clock.tick(60)
+        self.L[Tile_Num]="X"
 
-    def draw_O(self, Tile_Num, clock=None):
-        tl = tile_len
+    def draw_O(self, Tile_Num, clock=None, m=1):
+        tl = tile_len*m
         a = (tl // 2) - 10
         bigTile_Num=self.teller(Tile_Num)
         color2=TILE_INACTIVE
-        self.L[Tile_Num]="O"
+        
         
         if bigTile_Num==self.currentBigTile:color2=TILE_ACTIVE
+        if m!=1:color2=TILE_INACTIVE
         Tile = self.grid[Tile_Num]
         center = [int(Tile.x + (tl // 2)), int(Tile.y + (tl // 2))]
         c1 = [center[0] - a, center[1] - a]
         c2 = [center[0] + a, center[1] + a]
         c3 = [center[0] + a, center[1] - a]
         c4 = [center[0] - a, center[1] + a]
-        #T.drawActiveTiles()
         for i in range(7, (tl // 2) - 10, 2):
+            
             pygame.draw.rect(screen, color2, [Tile.x + 5, Tile.y + 5, tl - 10, tl - 10])
             pygame.draw.circle(screen, self.color, center, i, 6)
             pygame.draw.line(screen, color2, c1, c2, 7)
             pygame.draw.line(screen, color2, c3, c4, 7)
 
             if clock!=None:
-                T.makeGridLines(screen)
                 pygame.display.update()
                 clock.tick(60)
+        self.L[Tile_Num]="O"
 
     def drawAllSymbols(self):
-        
         for Tile_Num in range(len(self.L)):
             symbol=self.L[Tile_Num]
             if symbol==" ":continue
             if symbol=="X":self.draw_X(Tile_Num)
             if symbol=="O":self.draw_O(Tile_Num)
+
+    def drawAllBigSymbols(self):
+        tl=tile_len
+        for bigTile_Num in range(len(self.Big_L)):
+            bigTile_List=self.Dict[bigTile_Num]
+            s=self.Big_L[bigTile_Num]
+            coord=self.bigTile_coord[bigTile_Num]
+            
+            if s==" ":continue
+            
+            if s=="X":
+                pygame.draw.rect(screen, TILE_INACTIVE, [coord[0], coord[1], tl*3, tl*3])
+                self.draw_X(bigTile_List[0], m=3)
+
+            if s=="O":
+                pygame.draw.rect(screen, TILE_INACTIVE, [coord[0], coord[1], tl*3, tl*3])
+                self.draw_O(bigTile_List[0], m=3)
+
+            
+            
+        
 
     def getNextBigTile(self, Tile_Num):
         bigTile_List=self.Dict[self.teller(Tile_Num)]
@@ -229,6 +235,7 @@ class TicTacToe:
         text=False
         
         for bigTile_Num in self.Dict:
+            #if self.Big_L[bigTile_Num]!=" ":continue
             bigTile_List=self.Dict[bigTile_Num]
             for win in wins:
                 w1,w2,w3=win[0], win[1], win[2]
@@ -238,26 +245,9 @@ class TicTacToe:
                 s1, s2, s3= self.L[Tile_Num1], self.L[Tile_Num2], self.L[Tile_Num3]
                 s=s1+s2+s3
                 if s=="XXX":
-                    screen.fill(TILE_INACTIVE)
-                    self.draw_X(40)
-                    text="X"
-                    
+                    self.Big_L[bigTile_Num]="X"                    
                 if s=="OOO":
-                    screen.fill(TILE_INACTIVE)
-                    self.draw_O(40)
-                    text="O"
-
-        if text!=False:
-            
-            font=pygame.font.SysFont(None, 120)
-            t=font.render("WON", True, self.color)
-            screen.blit(t, [RES[0]//2-110, RES[1]//2+50])
-            return text
-        else:return False
-                
-                
-            
-            
+                    self.Big_L[bigTile_Num]="O"
         
 
 
@@ -312,7 +302,8 @@ while run:
             
             
     if T.currentBigTile==-1:continue
-    T.gridColor=AllColors2[0]
+    T.gridColor1=AllColors2[0]
+    T.gridColor2=AllColors2[50]
     T.color=AllColors[0]
     AllColors=cycle(AllColors)
     AllColors2=cycle(AllColors2)
@@ -321,10 +312,12 @@ while run:
     T.drawActiveTiles()
             
     T.drawAllSymbols()
+    
     T.makeGridLines(screen)
-
-    #Won=T.check_Winner()
     T.check_Winner()
+    T.drawAllBigSymbols()
+
+    
 
         
     pygame.display.update()
